51b3c96e0c5e55903d54f9b3de1de6b511d0c3a7
James Colannino <crankycyclops@gmail.com>
2015-03-03 22:41:13 -0800

    added a schematic and adjusted default lcd brightness

--- alarm/include/alarm.h
+++ alarm/include/alarm.h
@@ -11,7 +11,7 @@
 
 // defines how bright the LCD should be (valid values are 0-1023.)
 #define LCD_BRIGHTNESS_STEP      50
-#define LCD_BRIGHTNESS_DEFAULT  125
+#define LCD_BRIGHTNESS_DEFAULT   50
 
 // defines modes that the clock can be in
 enum ClockMode {DISPLAY_TIME, SET_TIME, DISPLAY_ALARM, SET_ALARM};
