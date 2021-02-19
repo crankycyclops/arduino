a8607860e0c2ae6138ca15350b692ff2280c53f2
James Colannino <james@colannino.dev>
2021-02-19 13:15:33 -0800

    modified blink to create a rolling pattern of lights

--- blink/blink.ino
+++ blink/blink.ino
@@ -1,17 +1,41 @@
 #include <Arduino.h>
 
+// A variation on the classic blink program. This version creates a simple
+// rolling pattern of lights.
+const int LED_1 = 2;
+const int LED_2 = 7;
+const int LED_3 = 4;
+
+/******************************************************************************/
+
 void setup() {
 
 	pinMode(LED_BUILTIN, OUTPUT);
+	pinMode(LED_1, OUTPUT);
+	pinMode(LED_2, OUTPUT);
+	pinMode(LED_3, OUTPUT);
+
+	digitalWrite(LED_BUILTIN, LOW);
+	digitalWrite(LED_1, LOW);
+	digitalWrite(LED_2, LOW);
+	digitalWrite(LED_3, LOW);
 }
 
 /******************************************************************************/
 
 void loop() {
 
+	digitalWrite(LED_3, LOW);
 	digitalWrite(LED_BUILTIN, HIGH);
-	delay(1000);
+	delay(500);
 	digitalWrite(LED_BUILTIN, LOW);
-	delay(1000);
+	digitalWrite(LED_1, HIGH);
+	delay(500);
+	digitalWrite(LED_1, LOW);
+	digitalWrite(LED_2, HIGH);
+	delay(500);
+	digitalWrite(LED_2, LOW);
+	digitalWrite(LED_3, HIGH);
+	delay(500);
 }
 
